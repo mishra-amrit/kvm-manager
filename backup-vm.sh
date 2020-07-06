28072749e187f0c46b7f4d9cc8800b9865dd08ff
Amrit Mishra <mishra.amrit@outlook.com>
2020-07-06 21:57:21 +0530

    updated backup script

--- backup-vm.sh
+++ backup-vm.sh
@@ -3,17 +3,33 @@
 TIMESTAMP=`date +%s`
 VM_NAME="centos8"
 VM_DISK_FILE="/var/lib/libvirt/backups/${VM_NAME}_${TIMESTAMP}_disk.qcow2"
-VM_MEM_FILE="/var/lib/libvirt/backups/${VM_NAME}_${TIMESTAMP}_mem.qcow2"
+VM_ARCHIVE_FILE="/var/lib/libvirt/backups/${VM_NAME}_${TIMESTAMP}.gz"
+
+#VM_MEM_FILE="/var/lib/libvirt/backups/${VM_NAME}_${TIMESTAMP}_mem.qcow2"
 VM_CONFIG_FILE="/var/lib/libvirt/backups/${VM_NAME}_${TIMESTAMP}_conf.xml"
 
+echo "dumping xml configuration file.."
 virsh dumpxml $VM_NAME > $VM_CONFIG_FILE
 
+echo "freezing filesystems on the vm.."
+virsh domfsfreeze $VM_NAME
+
+echo "creating snapshot.."
 virsh snapshot-create-as \
 	--domain $VM_NAME \
 	--name "bkp-${TIMESTAMP}" \
+	--disk-only \
 	--atomic \
 	--diskspec vda,file=$VM_DISK_FILE,snapshot=external \
-	--memspec file=$VM_MEM_FILE,snapshot=external \
 	--no-metadata
+echo "snapshot created."
 
+echo "archiving snapshot.."
+dd if=/var/lib/libvirt/images/centos8.qcow2 | gzip | dd of=$VM_ARCHIVE_FILE bs=4096
+echo "snapshot archived."
+
+echo "releasing filesystem."
+virsh domfsthaw $VM_NAME
+
+echo "performing blockcommit."
 virsh blockcommit $VM_NAME vda --active --pivot --verbose
