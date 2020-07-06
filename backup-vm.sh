b6625916792e7e6d858a94fec691b23c3bbf5680
Amrit Mishra <mishra.amrit@outlook.com>
2020-07-06 19:16:29 +0530

    script changes

--- backup-vm.sh
+++ backup-vm.sh
@@ -8,17 +8,13 @@ VM_CONFIG_FILE="/var/lib/libvirt/backups/${VM_NAME}_${TIMESTAMP}_conf.xml"
 
 virsh dumpxml $VM_NAME > $VM_CONFIG_FILE
 
-STATE=`virsh dominfo $VM_NAME | grep "State" | cut -d " " -f 11`
+virsh snapshot-create-as \
+	--domain $VM_NAME \
+	--name "bkp-${TIMESTAMP}" \
+	--atomic \
+	--quiesce \
+	--disk-only \
+	--diskspec vda,file=$VM_DISK_FILE,snapshot=external \
+	--no-metadata
 
-if [ "$STATE" = "running" ]; then
-	virsh snapshot-create-as \
-		--domain $VM_NAME $VM_BACKUP_NAME \
-		--memspec file=$VM_MEM_FILE,snapshot=external \
-		--live
-else
-	virsh snapshot-create-as \
-		--domain $VM_NAME $VM_BACKUP_NAME \
-		--diskspec sda,file=$VM_DISK_FILE,snapshot=external \
-		--disk-only \
-		--live
-fi
+virsh blockcommit $VM_NAME vda --active --pivot
--- create-vm.sh
+++ create-vm.sh
@@ -8,7 +8,7 @@ VM_MEMORY=2048
 VM_DISK_FILE=${LIBVIRT_HOME}/images/${VM_NAME}.qcow2
 VM_CFG_FILES="${WORKING_DIR}/cloudinit/user-data ${WORKING_DIR}/cloudinit/meta-data"
 VM_CONFIG_ISO_FILE=${LIBVIRT_HOME}/images/${VM_NAME}_cloudinit-cfg.iso
-
+VM_GUEST_AGENT=${LIBVIRT_HOME}/qemu/${VM_NAME}.agent
 # create cloud init cfg iso
 genisoimage -o ${VM_CONFIG_ISO_FILE} -V cidata -r -J ${VM_CFG_FILES}
 
@@ -17,6 +17,8 @@ qemu-img create -f qcow2 -o preallocation=metadata $VM_DISK_FILE ${VM_DISK_SIZE}
 
 # create vm using virt-install
 virt-install \
+	--hvm \
+	--accelerate \
 	--os-type generic \
 	--virt-type kvm \
 	--name $VM_NAME \
@@ -25,7 +27,6 @@ virt-install \
 	--boot cdrom,hd,network,menu=on \
 	--network bridge=br0,model=virtio \
 	--noautoconsole --graphics vnc,listen=0.0.0.0,port=5901,password=password \
-	--controller type=scsi,model=virtio-scsi \
-	--import \
-	--disk $VM_DISK_FILE,bus=scsi,format=qcow2 \
-	--cdrom $LIBVIRT_HOME/images/CentOS-8.2.2004-x86_64-dvd1.iso
+	--disk $VM_DISK_FILE,bus=virtio,format=qcow2 \
+	--cdrom $LIBVIRT_HOME/images/CentOS-8.2.2004-x86_64-dvd1.iso \
+	--channel unix,mode=bind,path=$VM_GUEST_AGENT,target_type=virtio,name=org.qemu.guest_agent.0
